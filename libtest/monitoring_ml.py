40b91d4097a881cf8a5360596bb74d2130ccec88
simran0413 <rrmsimran@gmail.com>
2022-06-07 16:09:18 +0200

    add test data invariants

--- libtest/monitoring_ml.py
+++ libtest/monitoring_ml.py
@@ -8,3 +8,16 @@
 
 
 # todo add more
+
+def data_invariants(feature_invariants_serving, value_range):
+    """
+        Checks that the feature invariants for the server model are within the same range as those in the training model
+        :params feature_invariants_serving: list of feature invariants each with a list of feature invariants values for all datapoints
+        :params value_range: list of tuples with the ranges for each feature invariant
+    """
+
+    for i, feature in enumerate(feature_invariants_serving):
+        for data in feature:
+            assert value_range[i][0] <= data <= value_range[i][1]
+
+
--- test/test_monitoring_ml.py
+++ test/test_monitoring_ml.py
@@ -1,7 +1,26 @@
 """
     ML Testing the StackOverflow label predictor for monitoring ML. Making use of the [todo library name] library.
 """
-import libtest.monitoring_ml
+import libtest.monitoring_ml as lib
+import joblib
 
 
 # todo add more
+def test_data_invariants():
+    X_train, _, _ = joblib.load("output/X_preprocessed.joblib")
+
+    min = 100
+    max = 0
+    input_lengths = []
+    for x in X_train:
+        length = len(x.split())
+        if length < min:
+            min = length
+        if length > max:
+            max = length
+        input_lengths.append(length)
+
+    lib.data_invariants([input_lengths], [(min, max)])
+
+
+
