8fc85f42ebbaafdf4363222e1c7f9d0aa875841f
Jord Molhoek <j.molhoek@student.tudelft.nl>
2022-06-09 20:02:52 +0200

    Fix some docstrings

--- libtest/features_and_data.py
+++ libtest/features_and_data.py
@@ -11,7 +11,11 @@ import numpy as np
 
 def no_unsuitable_features(used_features, unsuitable_features):
     """
-        Compares the list of used features to the list of unsuitable features. The size of the intersection should be 0.
+    Compares the list of used features to the list of unsuitable features. The size of the intersection should be 0.
+    :param used_features: List of used features (list of strings)
+    :param unsuitable_features: List of unsuitable features (list of strings)
+    Also works if both unused_features and unsuitable_features are lists of integers
+    (like index representations of features).
     """
     illegal_features = [f for f in used_features if f in unsuitable_features]
     assert len(illegal_features) == 0
@@ -19,9 +23,11 @@ def no_unsuitable_features(used_features, unsuitable_features):
 
 def feature_target_correlations(dataset, target, sample_size=10000):
     """"
-        Takes a matrix (#datapoints, #features) and a vector of targets (#datapoints).
-        Calculates the correlation of each individual feature with the target.
-        A sample of the points is taken for speedup
+    Calculates the correlation of each individual feature with the target.
+    A sample of the points is taken for speedup.
+    :param dataset: A numpy array of shape (#datapoints, #features)
+    :param target: A numpy array of shape (#datapoints)
+    :param sample_size: Size of the sample. Default is 10.000. Set to dataset.shape[0] to use all data.
     """
     n, f = dataset.shape
 
@@ -38,8 +44,11 @@ def feature_target_correlations(dataset, target, sample_size=10000):
 
 def pairwise_feature_correlations(dataset, sample_size=10000, feature_sample=5):
     """"
-        Takes a matrix (#datapoints, #features).
-        Calculates the correlation of each pair of features.
+    Calculates the correlation of each pair of features.
+    Takes a sample of the points and a sample of the features for speedup.
+    :param dataset: A numpy array of shape (#datapoints, #features)
+    :param sample_size: Size of the sample. Default is 10.000. Set to dataset.shape[0] to use all data.
+    :param feature_sample: Size of sample of features. Default is 5. Set to dataset.shape[1] to use all data.
     """
     n, f = dataset.shape
 
@@ -65,19 +74,29 @@ def pairwise_feature_correlations(dataset, sample_size=10000, feature_sample=5):
 
 def preprocessing_validation(examples, answers, preprocess_function, equals=lambda a, b: a == b):
     """
-        Asserts that preprocessing works.
+    Asserts that preprocessing works by assessing it on some examples with a known answer.
+    :param examples: List of examples to preprocess (of which the answers are known).
+    :param answers: List of correctly preprocessed data, corresponding to the examples on each index.
+    :param preprocess_function: The data preprocessing function under inspection
+    :param equals: Equality function to compare the results to the answers (per element)
     """
     for ex, ans in zip(examples, answers):
         assert equals(preprocess_function(ex), ans), f"Preprocessing went wrong for {ex}"
 
 
 def feature_values(dataset, feature_column_id, expected_values):
+    """
+    TODO ziad
+    """
     arr = dataset[:, feature_column_id].toarray().reshape(-1)
     for i in set(arr):
         assert i in expected_values
 
 
 def top_feature_values(dataset, feature_column_id, expected_values, topK=2, at_least_top_k_account_for=0.5):
+    """
+    TODO ziad
+    """
     arr = dataset[:, feature_column_id].toarray().reshape(-1)
     n_data = len(arr)
     features_distribution = collections.Counter(arr).most_common()
@@ -88,5 +107,3 @@ def top_feature_values(dataset, feature_column_id, expected_values, topK=2, at_l
         assert l in expected_values
         summation += r
     assert summation / n_data >= at_least_top_k_account_for
-
-# todo add more
