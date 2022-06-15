b68ea025d3ec8b3e53d4e10495a7c3322df4fa2f
abel <abel.van.steenweghen@gmail.com>
2022-06-15 14:48:01 +0200

    Fix import

--- src/serve_model.py
+++ src/serve_model.py
@@ -6,8 +6,8 @@ from flasgger import Swagger
 from flask import Flask, jsonify, request
 from scipy import sparse as sp_sparse
 
-from text_preprocessing import text_prepare
-from vectorization import my_bag_of_words, create_words_to_index
+from src.text_preprocessing import text_prepare
+from src.vectorization import my_bag_of_words, create_words_to_index
 
 app = Flask(__name__)
 swagger = Swagger(app)
